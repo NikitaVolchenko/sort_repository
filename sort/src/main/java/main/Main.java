ea4bf47c7e97ae73ac9a7f3aa5d8986b9b5a5fbe
tomik420z <malkovartem10@gmail.com>
2023-03-06 18:30:46 +0300

    реализована функция считывания фала с данными и занесения их в массив

--- sort/src/main/java/main/Main.java
+++ sort/src/main/java/main/Main.java
@@ -1,7 +1,26 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = null;
+        try (BufferedReader in = new BufferedReader(new FileReader("C:\\univer\\sort\\sort_repository\\data\\test.txt")))
+        {
+            array = in.lines().mapToInt(Integer::parseInt).toArray();
+        }
+        catch (IOException | NumberFormatException e)
+        {
+            e.printStackTrace();
+        }
+        for(int i = 0; i < array.length; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
     }
 }
