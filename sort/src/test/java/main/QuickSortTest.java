d5766569b48775091c6593d553946cf232dac3db
tomik420z <malkovartem10@gmail.com>
2023-03-06 16:19:11 +0300

    Реализованы ещё несколько тестов, рассмотрены краевые случаи

--- sort/src/test/java/main/QuickSortTest.java
+++ sort/src/test/java/main/QuickSortTest.java
@@ -13,6 +13,21 @@ public class QuickSortTest {
         int[] correctArr = {-2, 1, 4, 5, 5, 6};
         QuickSort.quickSort(arr, 0, arr.length - 1);
         assertArrayEquals(correctArr, arr);
+
+        int[] arr2 = {0, 0};
+        int[] correctArr2 = {0, 0};
+        QuickSort.quickSort(arr2, 0, arr2.length - 1);
+        assertArrayEquals(correctArr2, arr2);
+
+        int[] arr3 = {-1};
+        int[] correctArr3 = {-1};
+        QuickSort.quickSort(arr3, 0, arr3.length - 1);
+        assertArrayEquals(correctArr3, arr3);
+
+        int[] arr4 = {4,3,-2,-1};
+        int[] correctArr4 = {-2, -1, 3, 4};
+        QuickSort.quickSort(arr4, 0, arr4.length - 1);
+        assertArrayEquals(correctArr4, arr4);
     }
 }
 
